357fd9dd820082f4eddcaa857ac2171d86adda9f
supremex04 <bhandarisupreme7@gmail.com>
2024-05-02 07:46:07 +0545

    login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,14 @@ const Login = (props) => {
   return (
     <div className = "login-container">
       <h1 className = 'welcome-message'> Welcome to voting DApp</h1>
-      <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
+      <button class="button type--A" onClick={props.connectWallet}>
+        <div class="button__line"></div>
+        <div class="button__line"></div>
+        <span class="button__text">LOGIN</span>
+        <div class="button__drow1"></div>
+        <div class="button__drow2"></div>
+      </button>
+
     </div>
   )
 }
